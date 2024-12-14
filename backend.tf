f2fc8f89d2765ee7167d2080a77d84ace65205d0
bessax <andrebessax@gmail.com>
2024-12-14 19:51:04 -0300

    Ajustes

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "team-soat-bucket-bessa"
+    bucket = "team-soat-bucket"
     key    = "fast-order/tf/rds/terraform.tfstate"
     region = "us-east-1"
   }
