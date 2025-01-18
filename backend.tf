3db6e04945304449414382c8cab2657241d4d3c5
Darlei Randel <darley_rom@hotmail.com>
2025-01-18 13:11:55 -0300

    Refactor terraform files

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "team-soat-bucket-bessa"
-    key    = "fast-order/tf/rds/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
---
+++ iac/terraform/database/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "ACG-Terraform-Labs-Teste"
+
+    workspaces {
+      name = "techchallenge-database"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = ""
+  secret_key = ""
+  region     = "us-east-1"
+}
